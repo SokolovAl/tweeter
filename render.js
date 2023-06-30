db3aab267a7fe72a33891f3973495f6f9e856dc1
Aleksandr Sokolov <sokolovmono@gmail.com>
2023-06-30 17:31:06 +0300

    add function that create element for deleting post/comment change comment element from <ul> and <li> to <div> and <p> some changes in style.css

--- render.js
+++ render.js
@@ -15,10 +15,14 @@ const Renderer = (function () {
         const postTextElement = $("<h2>").addClass("post-text").text(post.text);
         postElement.append(postTextElement);
 
+        const deletePost = createDeleteElement("post");
+        postTextElement.append(deletePost);
+
         const commentsElement = $("<div>").addClass("comments");
         post.comments.forEach(comment => {
             const commentElement = createCommentElement(comment);
             commentsElement.append(commentElement);
+
         });
 
         postElement.append(commentsElement);
@@ -27,12 +31,27 @@ const Renderer = (function () {
     };
 
     const createCommentElement = (comment) => {
-        const commentElement = $("<ul>").addClass("comment").attr("data-id", comment.id);
+        const commentElement = $("<div>").addClass("comment").attr("data-id", comment.id);
 
-        const commentTextElement = $("<li>").text(comment.text);
+        const commentTextElement = $("<p>").text(comment.text);
         commentElement.append(commentTextElement);
 
+        const deletePost = createDeleteElement("comment");
+        commentTextElement.append(deletePost);
+
         return commentElement;
+    };
+
+    const createDeleteElement = (context) => {
+        let deleteElement;
+
+        if (context === "post") {
+            deleteElement = $("<span>").addClass("delete").text("X");
+        } else if (context === "comment") {
+            deleteElement = $("<span>").addClass("delete-comment").text("X");
+        }
+
+        return deleteElement;
     };
 
     return {
